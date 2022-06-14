b5dfe6fba25d1b914ad6466c81d7597db170de98
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-06-14 10:32:19 -0600

    update variables.js

--- variables.js
+++ variables.js
@@ -4,6 +4,12 @@ var worries = false
 
 var policeSound = "WOOP WOOP"
 var beastSound = true
-var runOutOfLyrics = 100
+var krs = 1
 
-//creating anotations!
+var stillDre = true
+var reppin = 213
+var lastAlbum = "The Chronic"
+
+// creating anotations with CTRL + /
+
+// This is where I would keep notes about how much I love hip-hop
